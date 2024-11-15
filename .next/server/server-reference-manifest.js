bc84b5151a181cb9466135ee0ca978a8c0795fbc
cproam <gdocs34@gmail.com>
2024-11-15 09:20:36 +0300

    лендинг список

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cQHcGY112BV/4/512tGYvHVlSW6kgOepUatwcGKILuo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RCY2XKRWiO0y7adWqKfUVYEYG+Q84CGn1nsuU6CPPUU=\"\n}"
