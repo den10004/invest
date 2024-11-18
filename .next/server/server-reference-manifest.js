56601bf8dab3c4001a5589b77cb185b134a9f957
cproam <gdocs34@gmail.com>
2024-11-18 10:13:05 +0300

    подкл шрифт

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qb6rjC8JmpzjLCaMncflM24Fw2cqGTXpWAcmIz5AnjM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"un7LxN7a7bEdSMGuTIJLwPVDMeAb2NJDB6Ha4PWmH6w=\"\n}"
--- app/layout.js
+++ app/layout.js
@@ -11,24 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <Head></Head>
 
       <body>
         <section id="main" className="main">
           <TheHeader /> <main>{children}</main>
         </section>
-
         <TheFooter />
       </body>
     </html>
