9218059a55fcd4598a788573adad82a81c76fe3a
cproam <gdocs34@gmail.com>
2024-11-19 12:31:54 +0300

    исправлен баннер лендинга

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e3XnC4n8dFan43SPjAuV4cP6Z9U30IiimdAtlJO6BwA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FA8KeqYbtI+Beb8G3fF0lZLhPtjZRYokZzqsMePdNUs=\"\n}"
