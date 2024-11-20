a3b7db0205706c13f6484a7e564892c88acd0051
cproam <gdocs34@gmail.com>
2024-11-20 09:11:45 +0300

    эксперимент со шрифтами

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9I2E/vILFEybC8UC3ei/NCbF21JLmIM5O0UNAHlLJG8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3+Ydb5dVrROQHkCj4OZSHkRNmllQxdSdaA6BA9Wwbaw=\"\n}"
